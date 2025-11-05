1fed73bbdaea1e70777c8770da7190ae49dd3e41
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-11-05 14:56:21 +1300

    feat: setup initial connection without rows

--- examples/react/trimmed/src/App.tsx
+++ examples/react/trimmed/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useStore } from '@tanstack/react-store';
 import { flexRender, useReactTable } from '@tanstack/react-table';
 import { createMosaicDataTableClient } from '@nozzle/mosaic-tanstack-table-core/trimmed';
@@ -24,41 +24,38 @@ export default App;
 
 const fileURL =
   'https://pub-1da360b43ceb401c809f68ca37c7f8a4.r2.dev/data/athletes.parquet';
-
+const tableName = 'athletes';
 const wasmConnector = vg.wasmConnector({ log: false });
 vg.coordinator().databaseConnector(wasmConnector);
 
 const $query = vg.Selection.intersect();
 
 function AthletesMosaic() {
-  const dataTable = useRef(
-    createMosaicDataTableClient('athletes', vg.coordinator()),
-  );
-  const store = useStore(dataTable.current.store);
-  const table = useReactTable(dataTable.current.getTableOptions(store));
-
+  const [isPending, setIsPending] = useState(true);
   const chartDivRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     if (!chartDivRef.current) return;
 
     async function setup() {
+      setIsPending(true);
+
       // Setup the Athletes Linear Regression Plot from https://idl.uw.edu/mosaic/examples/linear-regression.html
       await vg
         .coordinator()
         .exec([
-          `CREATE OR REPLACE TABLE athletes AS SELECT * FROM '${fileURL}'`,
+          `CREATE OR REPLACE TABLE ${tableName} AS SELECT * FROM '${fileURL}'`,
         ]);
 
       const plot = vg.plot(
-        vg.dot(vg.from('athletes'), {
+        vg.dot(vg.from(tableName), {
           x: 'weight',
           y: 'height',
           fill: 'sex',
           r: 2,
           opacity: 0.05,
         }),
-        vg.regressionY(vg.from('athletes', { filterBy: $query }), {
+        vg.regressionY(vg.from(tableName, { filterBy: $query }), {
           x: 'weight',
           y: 'height',
           stroke: 'sex',
@@ -70,8 +67,9 @@ function AthletesMosaic() {
         vg.xyDomain(vg.Fixed),
         vg.colorDomain(vg.Fixed),
       );
-
       chartDivRef.current?.replaceChildren(plot);
+
+      setIsPending(false);
     }
 
     setup();
@@ -86,52 +84,64 @@ function AthletesMosaic() {
       </div>
       <div>
         <h4>Table area</h4>
-        <table>
-          <thead>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <th key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext(),
-                        )}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody>
-            {table.getRowModel().rows.map((row) => (
-              <tr key={row.id}>
-                {row.getVisibleCells().map((cell) => (
-                  <td key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-          <tfoot>
-            {table.getFooterGroups().map((footerGroup) => (
-              <tr key={footerGroup.id}>
-                {footerGroup.headers.map((header) => (
-                  <th key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.footer,
-                          header.getContext(),
-                        )}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </tfoot>
-        </table>
+        {isPending ? <div>Loading data...</div> : <AthletesTable />}
       </div>
     </div>
+  );
+}
+
+function AthletesTable() {
+  const dataTable = useRef(
+    createMosaicDataTableClient(tableName, { coordinator: vg.coordinator() }),
+  );
+  const store = useStore(dataTable.current.store);
+  const table = useReactTable(dataTable.current.getTableOptions(store));
+
+  return (
+    <table>
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+      <tfoot>
+        {table.getFooterGroups().map((footerGroup) => (
+          <tr key={footerGroup.id}>
+            {footerGroup.headers.map((header) => (
+              <th key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.footer,
+                      header.getContext(),
+                    )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </tfoot>
+    </table>
   );
 }
--- packages/mosaic-tanstack-table-core/src/trimmed/data-table.ts
+++ packages/mosaic-tanstack-table-core/src/trimmed/data-table.ts
@@ -1,24 +1,20 @@
-import { MosaicClient } from '@uwdata/mosaic-core';
+import {
+  MosaicClient,
+  queryFieldInfo,
+  toDataColumns,
+} from '@uwdata/mosaic-core';
+import { Query } from '@uwdata/mosaic-sql';
 import { getCoreRowModel } from '@tanstack/table-core';
 import { Store, batch } from '@tanstack/store';
-import type { Coordinator, Selection } from '@uwdata/mosaic-core';
+
+import type { Coordinator, FieldInfo, Selection } from '@uwdata/mosaic-core';
+import type { FilterExpr, SelectQuery } from '@uwdata/mosaic-sql';
 import type { ColumnDef, TableOptions, TableState } from '@tanstack/table-core';
 
-/**
- * let client = new DataTable({
-		table,
-		schema: empty.schema,
-		height: options.height,
-	});
-	options.coordinator.connect(client);
- */
-
 export interface MosaicDataTableOptions {
+  coordinator: Coordinator;
   filterBy?: Selection | undefined;
 }
-
-export type MosaicQueryMethodArg = Parameters<MosaicClient['query']>[0];
-export type MosaicQueryMethodReturn = ReturnType<MosaicClient['query']>;
 
 export type MosaicDataTableRow = {
   id: string;
@@ -26,45 +22,44 @@ export type MosaicDataTableRow = {
 };
 
 export type MosaicDataTableStore = {
-  demoState: { foo: string };
   tableState: TableState;
-  rows: Array<MosaicDataTableRow>;
+  arrowColumnSchema: Array<FieldInfo>;
+  rows: Array<Record<string, any>>;
 };
 
 export function createMosaicDataTableClient(
   tableName: string,
-  coordinator: Coordinator,
-  options?: MosaicDataTableOptions,
+  options: MosaicDataTableOptions,
 ) {
   const client = new MosaicDataTable(tableName, options);
 
-  coordinator.connect(client);
+  options.coordinator.connect(client);
 
   return client;
 }
 
 export class MosaicDataTable extends MosaicClient {
-  dataTableName = '';
+  from = '';
+  columns = ['*'];
+
+  schema: Array<FieldInfo> = [];
+
   #store: Store<MosaicDataTableStore>;
 
-  constructor(tableName: string, options?: MosaicDataTableOptions) {
-    super(options?.filterBy); // pass appropriate filterSelection if needed
+  constructor(tableName: string, options: MosaicDataTableOptions) {
+    super(options.filterBy); // pass appropriate filterSelection if needed
+    this.coordinator = options.coordinator;
+
     if (!tableName) {
       throw new Error('[MosaicDataTable] A table name must be provided.');
     }
 
-    this.dataTableName = tableName;
-
+    this.from = tableName;
     this.#store = new Store(
       {
-        demoState: { foo: 'bar' },
         tableState: seedInitialTableState(),
-        rows: [
-          {
-            id: '1',
-            name: 'Alice',
-          },
-        ] satisfies MosaicDataTableStore['rows'],
+        rows: [] as MosaicDataTableStore['rows'],
+        arrowColumnSchema: [] as MosaicDataTableStore['arrowColumnSchema'],
       },
       {
         onUpdate: () => {},
@@ -72,10 +67,55 @@ export class MosaicDataTable extends MosaicClient {
     );
   }
 
-  // override query(filter?: MosaicQueryMethodArg): MosaicQueryMethodReturn {
-  //   // Implement query logic specific to the data table
-  //   return undefined as any;
-  // }
+  override query(filter?: FilterExpr | null | undefined): SelectQuery {
+    // console.debug('[MosaicDataTable] query() Executing query with filter:', filter);
+    const pagination = this.#store.state.tableState.pagination;
+    const offset = pagination.pageIndex * pagination.pageSize;
+
+    const result = Query.from(this.from)
+      .select(['*'])
+      .limit(pagination.pageSize)
+      .offset(offset);
+
+    console.debug('[MosaicDataTable] query() Generated query:', result);
+    return result;
+  }
+
+  override queryPending(): this {
+    return this;
+  }
+
+  override queryError(error: Error): this {
+    console.error('[MosaicDataTable] queryError() Query error:', error);
+    return this;
+  }
+
+  override queryResult(data: unknown): this {
+    if (data) {
+      const dataColumns = toDataColumns(data);
+      console.debug(
+        '[MosaicDataTable] queryResult() Converted data columns:',
+        dataColumns,
+      );
+    }
+
+    batch(() => {
+      this.#store.setState((prev) => {
+        return { ...prev, arrowColumnSchema: this.schema };
+      });
+    });
+
+    return this;
+  }
+
+  override async prepare(): Promise<void> {
+    const table = this.from;
+    const fields = this.columns.map((column) => ({ column, table }));
+    const schema = await queryFieldInfo(this.coordinator!, fields);
+    this.schema = schema;
+
+    return Promise.resolve();
+  }
 
   /**
    * Get the TanStack Table options to be used with the framework adapters.
@@ -83,14 +123,12 @@ export class MosaicDataTable extends MosaicClient {
   getTableOptions(
     state: Store<MosaicDataTableStore>['state'],
   ): TableOptions<unknown> {
-    const columns = [
-      {
-        accessorKey: 'id',
-      },
-      {
-        accessorKey: 'name',
-      },
-    ] satisfies Array<ColumnDef<MosaicDataTableRow>>;
+    const columns = state.arrowColumnSchema.map((field) => {
+      return {
+        accessorKey: field.column,
+        header: field.column,
+      } satisfies ColumnDef<MosaicDataTableRow>;
+    });
 
     return {
       data: state.rows,
@@ -110,26 +148,6 @@ export class MosaicDataTable extends MosaicClient {
       },
       manualPagination: true,
     };
-  }
-
-  addRow(name: string): void {
-    const newId = (this.#store.state.rows.length + 1).toString();
-    const newRow = { id: newId, name };
-
-    batch(() => {
-      this.#store.setState((prev) => ({
-        ...prev,
-        rows: [...prev.rows, newRow],
-      }));
-    });
-  }
-
-  mutateStateTo(value: string): void {
-    batch(() => {
-      this.#store.setState((prev) => {
-        return { ...prev, demoState: { foo: value } };
-      });
-    });
   }
 
   get store(): Store<MosaicDataTableStore> {
