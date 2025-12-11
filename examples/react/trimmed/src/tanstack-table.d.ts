5183d21b0430c86a459e2ea1819836d27f5503b8
christiancasazza <christiancasazza99@gmail.com>
2025-12-11 10:33:43 -0500

    add paa dash

--- examples/react/trimmed/src/tanstack-table.d.ts
+++ examples/react/trimmed/src/tanstack-table.d.ts
@@ -2,6 +2,7 @@ import '@tanstack/react-table';
 import type { MosaicDataTableColumnDefMetaOptions } from '@nozzleio/mosaic-tanstack-react-table';
 
 declare module '@tanstack/react-table' {
+  // eslint-disable-next-line unused-imports/no-unused-vars
   interface ColumnMeta<TData extends RowData, TValue>
     extends MosaicDataTableColumnDefMetaOptions {
     filterVariant?: 'text' | 'range' | 'select';
