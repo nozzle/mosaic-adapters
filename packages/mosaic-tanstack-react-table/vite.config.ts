8a26bb2e167a19fc23f47daae876a9fb90ff891d
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2026-01-14 13:07:13 +1300

    build: add and configure `vitest` for unit testing
    this commit adds and configures vitest in this monorepo
    for running unit test suites on the packages

--- packages/mosaic-tanstack-react-table/vite.config.ts
+++ packages/mosaic-tanstack-react-table/vite.config.ts
@@ -1,9 +1,18 @@
-import { defineConfig, mergeConfig } from 'vite';
+import { defineConfig, mergeConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { tanstackViteConfig } from '@tanstack/vite-config';
 
+import packageJson from './package.json';
+
 const packageConfig = defineConfig({
   plugins: [react()],
+  test: {
+    name: packageJson.name,
+    dir: './tests',
+    watch: false,
+    environment: 'jsdom',
+    typecheck: { enabled: true },
+  },
 });
 
 export default mergeConfig(
